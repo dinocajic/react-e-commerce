c9f78744040ddbf9932a505a72da2eccf462ebd3
Dino Cajic <dinocajic@gmail.com>
2020-11-09 11:13:19 -0500

    created home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Header from './components/layout/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* Home */}
+      <Home />
 
     </div>
   );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Home.css';
+
+import Banner from './img/banner.jpg';
+
+function Home() {
+    return (
+        <div className='home'>
+            <div className='home_container'>
+                <img 
+                    src={Banner} 
+                    alt='Home Banner' 
+                    class='home_image'
+                />
+            </div>
+
+            <div className='home_row'>
+                
+            </div>
+
+            <div className='home_row'>
+
+            </div>
+
+            <div className='home_row'>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Home
