026da1ba12ae2f361ad428051b0e320724a30626
Dino Cajic <dinocajic@gmail.com>
2020-11-09 12:52:49 -0500

    added router to App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import './App.css';
 import Header from './components/layout/Header';
 import Home from './components/Home/Home';
 
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path='/' exact>
+            <Header />
+            <Home />
+          </Route>
 
-    </div>
+          <Route path='/checkout'>
+            <Header />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
