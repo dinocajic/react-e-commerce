905ee27d18e2776b8b417ca648bc9e5ff6985fbd
Dino Cajic <dinocajic@gmail.com>
2020-11-10 14:56:57 -0500

    added state to email/password and onClick functionality to buttons

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,27 @@
+import React, { useState } from 'react';
 import { Button } from '@material-ui/core';
-import React from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css';
 
 function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        e.preventDefault();
+
+        // Firebase Login
+        console.log(email);
+    };
+
+    const register = e => {
+        e.preventDefault();
+
+        // Firebase Register
+        console.log(email);
+    };
+
     return (
         <div className='login'>
             <Link to='/'>
@@ -19,15 +37,27 @@ function Login() {
                 
                 <form>
                     <h5>Email</h5>
-                    <input type='text' name='email' />
+                    <input 
+                        type='text' 
+                        name='email' 
+                        value={email} 
+                        onChange={e => setEmail(e.target.value)}
+                    />
 
                     <h5>Password</h5>
-                    <input type='password' name='password' />
+                    <input 
+                        type='password' 
+                        name='password' 
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
 
                     <Button 
                         variant='outlined' 
                         color='primary' 
-                        size='large'
+                        size='large' 
+                        type='submit'
+                        onClick={signIn}
                     >
                         Sign In
                     </Button>
@@ -38,7 +68,10 @@ function Login() {
                     the Privacy Policy page.
                 </p>
 
-                <button className='login_registerButton'>
+                <button 
+                    className='login_registerButton'
+                    onClick={register}
+                >
                     Create Account
                 </button>
             </div>
