9ed30780f6333a41c4a7786957c9e05a518cca5a
Dino Cajic <dinocajic@gmail.com>
2020-11-10 08:14:48 -0500

    added dynamic cart count based on cart state in header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,8 +3,12 @@ import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import { Link } from 'react-router-dom';
+import { useStateValue } from '../../StateProvider';
 
 function Header() {
+
+    const [{cart}, dispatch] = useStateValue();
+
     return (
         <div className='header'>
             <Link to='/'>
@@ -43,7 +47,7 @@ function Header() {
             <Link to='/checkout'>
                 <div className='header_optionCart'>
                     <ShoppingBasketIcon />
-                    <span className='header_optionLineTwo header_cartCount'>0</span>
+                    <span className='header_optionLineTwo header_cartCount'>{cart?.length}</span>
                 </div>
             </Link>
         </div>
