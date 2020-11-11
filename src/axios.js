9d1505d3130c9870ba6854460578d2c06935ab68
Dino Cajic <dinocajic@gmail.com>
2020-11-11 15:04:55 -0500

    updated api url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/e-commerce-9961f/us-central1/api', // API URL (cloud function)
+    // baseURL: 'http://localhost:5001/e-commerce-9961f/us-central1/api', // API URL (cloud function)
+    baseURL: 'https://us-central1-e-commerce-9961f.cloudfunctions.net/api'
 });
 
 export default instance;
